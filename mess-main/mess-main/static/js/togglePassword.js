15fa9c6910e30e355ab3e6f681deb26d72b96d47
kihaas <152589310+kihaas@users.noreply.github.com>
2024-12-10 23:25:17 +0400

    Delete mess-main/mess-main directory

--- mess-main/mess-main/static/js/togglePassword.js
+++
@@ -1,8 +0,0 @@
-function togglePassword(inputId) {
-    const input = document.getElementById(inputId);
-    if (input.type === "password") {
-        input.type = "text";
-    } else {
-        input.type = "password";
-    }
-}
